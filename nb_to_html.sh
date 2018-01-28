a726b991c743ec40b9fc9ea0bf7f6104a9874699
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2018-01-28 11:04:22 +0100

    Rename jupyter-notebook
    because it is used as title in the output html file

--- nb_to_html.sh
+++ nb_to_html.sh
@@ -2,4 +2,4 @@
 # I am writing a jupyter-notebook and convert it
 #   programmatically to html which is then displayed
 #   on my github-page
-jupyter-nbconvert webpage-template.ipynb --to html --output index.html
+jupyter-nbconvert Code\ Portfolio\ -\ Adrian\ Köring.ipynb  --to html --output index.html
