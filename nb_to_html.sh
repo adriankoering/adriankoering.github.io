92ba7aedba1490c6211ee0115e0fe15cf47b5f36
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2018-01-28 10:59:11 +0100

    Add script to convert jupyter-notebooks to html

---
+++ nb_to_html.sh
@@ -0,0 +1,5 @@
+# Kind of a static page generation.
+# I am writing a jupyter-notebook and convert it
+#   programmatically to html which is then displayed
+#   on my github-page
+jupyter-nbconvert webpage-template.ipynb --to html --output index.html
